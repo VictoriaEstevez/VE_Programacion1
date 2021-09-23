d8445ba4d7ebfa46ab315c7dca71513cd17250b6
VictoriaEstevez <estevez.victoria00@gmail.com>
2021-09-23 17:09:34 -0300

    CRUD

--- Firebase-ej1/main.js
+++ Firebase-ej1/main.js
@@ -15,6 +15,41 @@ const guardarCliente = async () => {
     document.getElementById("inp_nom").value= '';
     document.getElementById("inp_ape").value= '';
     document.getElementById("inp_dni").value= '';
+    listar_clientes();
 }
 const boton = document.getElementById("btn_obtener")
 boton.addEventListener("click",guardarCliente)
+
+//eliminar cliente
+function eliminar(id){
+    db.collection("Clientes").doc(id).delete()
+    listar_clientes();
+}
+const listar_clientes = async() =>{
+    const clientes = await db.collection("Clientes").get();
+
+    let lista_clientes = []
+    
+    const lista = clientes.docs.map(doc =>{
+        lista_clientes = doc.data()
+        lista_clientes.id = doc.id;
+        return lista_clientes;
+    });
+    let total_filas = []
+    lista.forEach(element =>{
+        let fila= `
+            <tr>
+                <td>${element.nombre}</td> 
+                <td>${element.apellido}</td> 
+                <td>${element.dni}</td> 
+                <td>
+                <button onclick="eliminar('${element.id}')"class="btn btn-danger btn-sa"> <i class="fa fa-trash"></i> </button>
+                </td> 
+            </tr>     
+        `
+        total_filas.push(fila)
+    });
+    document.getElementById("tbody").innerHTML=total_filas.join(``)
+}
+//ejercutar funcion
+listar_clientes()
